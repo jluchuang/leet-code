24b8f0fd086c0f75d6017fae279b986a43b73e2b
jluchuang <jlu_chuang@163.com>
2016-11-04 21:00:25 +0800

    Add Revert Tree with java
    N/A
    
    Signed-off-by: jluchuang <jlu_chuang@163.com>

---
+++ RevertTree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) {
+            return null; 
+        }
+        
+        TreeNode right = root.right; 
+        root.right = invertTree(root.left); 
+        root.left = invertTree(right); 
+        
+        return root; 
+    }
+}
