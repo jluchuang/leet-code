0b0c07eaba8564bd28e39b47ccd0298e95c4723c
jluchuang <jlu_chuang@163.com>
2016-12-03 13:51:26 +0800

    Add First Bad Version with java binary search
    N/A
    
    Signed-off-by: jluchuang <jlu_chuang@163.com>

---
+++ FirstBadVersion.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        // Binary Search
+        int st = 1, ed = n;
+        while (st < ed) {
+            int mid = st + (ed-st) / 2; 
+            
+            if(isBadVersion(mid)) {
+                ed = mid; 
+            }
+            else {
+                st = mid + 1;
+            }
+            
+        }
+        return st; 
+    }
+}
